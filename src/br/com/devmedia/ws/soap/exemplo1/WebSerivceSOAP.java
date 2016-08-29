b378cf2b4dcbec9cfc43b44f5b1e103531a6a056
Carlos A. Schuenck J <carlosschuenck@hotmail.com>
2016-08-29 15:06:41 -0300

    Revert "commit"
    This reverts commit ea52ce4dfc719b6efd73e273b537504e26f3f37e.

--- src/br/com/devmedia/ws/soap/exemplo1/WebSerivceSOAP.java
+++ src/br/com/devmedia/ws/soap/exemplo1/WebSerivceSOAP.java
@@ -1,15 +1,32 @@
-/**
- * WebSerivceSOAP.java
- *
- * This file was auto-generated from WSDL
- * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
- */
-
 package br.com.devmedia.ws.soap.exemplo1;
 
-public interface WebSerivceSOAP extends java.rmi.Remote {
-    public java.lang.String ping(java.lang.String nome) throws java.rmi.RemoteException;
-    public java.lang.Integer mostrarIdade() throws java.rmi.RemoteException;
-    public java.lang.String[] getListarString() throws java.rmi.RemoteException;
-    public br.com.devmedia.ws.soap.exemplo1.Carro[] getListarCarros() throws java.rmi.RemoteException;
+import java.util.Random;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.validation.constraints.NotNull;
+
+@WebService
+public class WebSerivceSOAP {
+	
+	@WebMethod
+	/**
+	 * WebMethod indica que o método vai ser publicado, não é obrigatório, se não colocar
+	 * o método vai ser publicado automaticamente, mas se quiser não publicar, é 
+	 * só setar o exclude para true
+	 * @param nome
+	 * @return
+	 */
+	public String ping(@WebParam(name="nome") @NotNull String nome){
+		return "Pong! : "+nome;
+	}
+	@WebMethod(operationName="MostrarIdade",exclude=false)
+	public Integer getAge(){
+		return new Random().nextInt();
+	}
+	
+	private Integer getAgeteste(){
+		return new Random().nextInt();
+	}
 }
